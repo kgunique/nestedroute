c43c8233ec3bbe5e542ccece595d38a278eb64e3
Karan <96karancpp@gmail.com>
2022-01-22 00:34:53 +0530

    adding search params

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,12 @@ import { Link, Outlet } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <nav style={{display:'flex',alignItems:'center',justifyContent:'space-between'}}>
+      <nav style={{display:'flex',alignItems:'center',justifyContent:'space-evenly',height:'2rem',backgroundColor:'cyan'}}>
         <Link to="/home">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/product">Product</Link>
+        <Link to="/users">User</Link>
+        
       </nav>
     <Outlet/>
     </div>
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 const Users = () => {
-  return <div>
+  const [searchParams, setSearchParams] = useSearchParams();
+  const activeUsers = searchParams.get("filter") === "active";
+  const disableUsers = searchParams.get("filter") === "disable";
+
+  return (
+    <div>
       <h1>User 1</h1>
       <h1>User 2</h1>
       <h1>User 3</h1>
-    <Outlet/>
-  </div>;
-  
+      <Outlet />
+      <div>
+        <button onClick={() => setSearchParams({ filter: "active" })}>
+          Active Users
+        </button>
+        <button onClick={() => setSearchParams({ filter: "disable" })}>
+          disable Users
+        </button>
+        <button onClick={() => setSearchParams({})}>Reset User</button>
+      </div>
+
+      {activeUsers ? (
+        <h1>Showing Active Users</h1>
+      ) : disableUsers ? (
+        <h1>Showing Disabled Users</h1>
+      ) : (
+        <h1>Showing all Users</h1>
+      )}
+    </div>
+  );
 };
 
 export default Users;
